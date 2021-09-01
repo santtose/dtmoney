6eb699222606ec6c7eecea8ffe9a37dfca1641dc
santtose <santtose@outlook.com>
2021-09-01 15:52:41 -0300

    listing transactions and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-09-01 03:45:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-08-01 11:45:00')
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
