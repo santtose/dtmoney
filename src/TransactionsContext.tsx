67c1df8c6e6b9be6e29f009a41850dd0187547c2
santtose <santtose@outlook.com>
2021-09-02 16:32:51 -0300

    inserting data

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -25,7 +25,7 @@ interface TransactionsProviderProps {
 
 interface TransactionContextData {
   transactions: Transaction[];
-  createTransaction: (Transaction: TransactionInput) => void;
+  createTransaction: (Transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionContextData>(
@@ -40,8 +40,17 @@ export function TransactionProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction,      
+    ]);
   }
 
   return (
