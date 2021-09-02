99dfa9878a5bb5383dfb781c479b00cb4c64bfda
santtose <santtose@outlook.com>
2021-09-02 16:11:45 -0300

    introducing contextAPI

---
+++ src/TransactionsContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useEffect, useState, ReactNode } from 'react';
+import { api } from './services/api';
+
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt:string;
+}
+
+// interface TransactionInput {
+//   title: string;
+//   amount: number;
+//   type: string;
+//   category: string;
+// }
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+interface TransactionContextData {
+  transactions: Transaction[];
+  createTransaction: (Transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionContextData>(
+  {} as TransactionContextData  
+);
+
+export function TransactionProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, []);
+
+  function createTransaction(transaction: TransactionInput) {
+    api.post('/transactions', transaction);
+  }
+
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
